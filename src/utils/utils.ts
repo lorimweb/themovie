e886433685a1603bbc90a49372ed46a8b0b2c563
Clenilson Lopes <lorimweb@gmail.com>
2025-03-28 17:36:26 -0300

    refactor: remove unused languageItems and related imports from utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,23 +1,6 @@
-import { MenuProps } from "antd";
-
 export const scrollToTop = (top: number, behavior: ScrollBehavior) => {
   window.scrollTo({
     top: top,
     behavior: behavior ? behavior : 'smooth'
   });
 };
-
-export const languageItems: MenuProps['items'] = [
-  {
-    key: 'en',
-    label: 'English',
-  },
-  {
-    key: 'es',
-    label: 'Español',
-  },
-  {
-    key: 'pt',
-    label: 'Português',
-  },
-];
